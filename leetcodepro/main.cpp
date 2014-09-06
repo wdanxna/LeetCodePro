c95d289bb356a97e003242bf7a1f4dad337d21a2
drakeDan <drakedan@drakeDans-MacBook-Pro.local>
2014-09-06 23:05:23 +0800

    Initial Commit

---
+++ leetcodepro/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  leetcodepro
+//
+//  Created by drakeDan on 9/6/14.
+//  Copyright (c) 2014 bravo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
